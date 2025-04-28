543dadf7ec7e9d5d97ae0e13a50b3ce709ad1d07
Georgy451 <wiktory1975@gmail.com>
2025-04-28 22:03:14 +0300

    Базовая настройка

--- music/song/views.py
+++ music/song/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from .models import * 
 
-# Create your views here.
+def index(request):
+    return render(request,'song/index.html',)
+
+
+
+
