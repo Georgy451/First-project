54adba84d8488601751749279ddeda0f72da7d77
Georgy451 <wiktory1975@gmail.com>
2025-05-05 01:20:55 +0300

    NEW HTML

--- music/song/urls.py
+++ music/song/urls.py
@@ -3,4 +3,10 @@ from . import views
 urlpatterns = [
 path('', views.index, name='home'),
 path('base', views.base, name='base'),
+path('profile', views.profile, name='profile'),
+path('about', views.about, name='about'),
+path('search', views.search, name='search'),
+path('profile', views.profile, name='profile'),
+
+
 ]
--- music/song/views.py
+++ music/song/views.py
@@ -9,6 +9,30 @@ def index(request):
 def base(request):
     return render(request,'song/base.html',)
 
+def profile(request):
+    context = {
+        'username': 'МузыкальныйЭнтузиаст',
+        'bio': 'Слушаю и создаю музыку',
+        'tracks_count': 3,
+        'followers_count': 42,
+        'following_count': 13,
+        'user_tracks': [
+            {'title': 'Мой первый трек', 'artist': 'Я', 'plays': 100},
+            {'title': 'Летний бит', 'artist': 'Я', 'plays': 75},
+            {'title': 'Осенняя мелодия', 'artist': 'Я', 'plays': 50},
+        ]
+    }
+    return render(request, 'song/profile.html', context)
+
+def search(request):
+    return render(request, 'song/search.html')
+
+
+def about(request):
+    return render(request, 'song/about.html')
+
+
+
 
 
 
